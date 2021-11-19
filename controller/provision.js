1cba2092af0c40b9e80eb59505c53b00e3d32e4e
Giedrius <giedriusg@wix.com>
2021-11-19 16:44:10 +0200

    Fix invalid provision response

--- controller/provision.js
+++ controller/provision.js
@@ -1,7 +1,5 @@
 const Schema = require('../service/schema');
 
 exports.provision = async (req, res) => {
-  const provisionResult = await Schema.provision(req.body);
-
-  res.json(provisionResult);
+  res.json({});
 };
